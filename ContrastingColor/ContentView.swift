67cfacbc4b07adef0fa3ed0ebda62130c3ede914
Michel Storms <stormychel@me.com>
2024-10-14 16:15:05 +0200

    Initial Commit

---
+++ ContrastingColor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContrastingColor
+//
+//  Created by Michel Storms on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContrastingColor/ContrastingColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContrastingColorApp.swift
+//  ContrastingColor
+//
+//  Created by Michel Storms on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ContrastingColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
